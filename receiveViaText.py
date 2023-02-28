b3df3c832dcbd86dd446e34fdc2d791cf0b52fb5
Jack Hollingsworth <jackhhollings@gmail.com>
2023-02-28 10:40:39 -0500

    added memories prompt

--- receiveViaText.py
+++ receiveViaText.py
@@ -17,10 +17,10 @@ def askGPT(prompt): #pass prompt to ChatGPT and print response
     split_response = response.split(':') #take big response with both replies and divide by colons
     finalResponse = split_response[2].strip() #take all information after the second colon as final response
     print("OuijaGPT: ", finalResponse) #print final response to console
-    #SpeakText(finalResponse) #DEBUG: speak final repsonse, this is placeholder for spelling on the board
+    SpeakText(finalResponse) #DEBUG: speak final repsonse, this is placeholder for spelling on the board
 
 def ouijaPrompt(): #pass Ouija jailbreak prompt to ChatGPT instance
-  with open('ouijaPrompt.txt', 'r', encoding="utf8") as file:
+  with open('memoriesPrompt.txt', 'r', encoding="utf8") as file:
     jailbreakPrompt = file.read().rstrip('\n') #store big character prompt in string
   response = bot.ask(jailbreakPrompt) #initialize "Ouija Mode" in ChatGPT by removing OpenAI restrictions
   #print(response) #verify that Ouija Mode has successfully been activated with print to console
